9a5e8eac45b46306bea8506991e742e37db5e15f
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-12 22:51:47 +0300

    fix: render

--- src/lib/renderContent.tsx
+++ src/lib/renderContent.tsx
@@ -1,8 +1,8 @@
 import * as ReactDOMServer from 'react-dom/server';
 import {SendMessageProps} from "../types";
 import React = require('react');
-import {CustomRenderer} from "../component-blocks/CustomRenderer/CustomRenderer";
+import {DocumentRenderer} from "@keystone-6/document-renderer";
 
 export const renderContent = (content: SendMessageProps['content']) => {
-    return  ReactDOMServer.renderToString(<CustomRenderer document={content} />);
+    return  ReactDOMServer.renderToString(<DocumentRenderer document={content} />);
 }
