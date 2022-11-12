54a22cde845a0eb7baafde5ad2ba2e6180fb183d
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-12 22:48:08 +0300

    fix: render

--- src/component-blocks/CustomRenderer/CustomRenderer.tsx
+++ src/component-blocks/CustomRenderer/CustomRenderer.tsx
@@ -8,47 +8,6 @@ import { YouTubeVideo } from '../YouTubeVideo/YouTubeVideo';
 
 type CustomRendererProps = ComponentProps<typeof DocumentRenderer>;
 
-const defaultElementRenderers: CustomRendererProps['renderers'] = {
-  block: {
-    block: React.Fragment,
-    blockquote({ children }) {
-      return <blockquote style={{
-        margin: 0,
-        borderLeft: "5px solid #D1D0CE",
-        padding: "8px 24px"
-      }}>{children}</blockquote>;
-    },
-    code({ children }) {
-      return <pre style={{
-        fontFamily: `Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Monaco, "Courier New", Courier, monospace`,
-        whiteSpace: "pre-wrap",
-        overflowX: "auto",
-        backgroundColor: "#2c3e50",
-        color: "whitesmoke",
-        padding: 16,
-        fontSize: "1.25rem",
-        borderRadius: 8
-
-      }}>{children}</pre>;
-    },
-  },
-  inline: {
-    bold: ({ children }) => {
-      return <strong style={{ color: '#363945' }}>{children}</strong>;
-    },
-    code: ({ children }) => {
-      return <code style={{
-        fontFamily: "monospace",
-        wordWrap: "break-word",
-        boxDecorationBreak: "clone",
-        padding: ".1rem .3rem .2rem",
-        borderRadius: 4,
-        backgroundColor: "#eee"
-      }}>{children}</code>;
-    },
-  },
-};
-
 const customComponentRenderers: CustomRendererProps['componentBlocks'] = {
   hero: props => {
     return <Hero {...props} />;
