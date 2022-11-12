5a35df4cadb0f87475c515fb45676e5db1c6ff95
Nikolay Kamenev <kamenik1@icloud.com>
2022-11-12 22:34:57 +0300

    feat: added custom render

--- src/lib/renderContent.tsx
+++ src/lib/renderContent.tsx
@@ -1,8 +1,8 @@
 import * as ReactDOMServer from 'react-dom/server';
 import {SendMessageProps} from "../types";
 import React = require('react');
-import {DocumentRenderer} from "@keystone-6/document-renderer";
+import {CustomRenderer} from "../component-blocks/CustomRenderer/CustomRenderer";
 
 export const renderContent = (content: SendMessageProps['content']) => {
-    return  ReactDOMServer.renderToString(<DocumentRenderer document={content} />);
+    return  ReactDOMServer.renderToString(<CustomRenderer document={content} />);
 }
